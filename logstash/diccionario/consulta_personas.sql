d1115148d62be904e160a3cc06b9bcf8f85c3cc4
jmartinezl-ASD <jmartinez@grupoasd.com>
2024-02-22 16:36:48 -0500

    1 try

---
+++ logstash/diccionario/consulta_personas.sql
@@ -0,0 +1,12 @@
+SELECT
+    p.id_personas, 
+    p.role,
+    p.nombreusuario,  
+    p.nombre1,
+    p.nombre2, 
+    p.apellido1, 
+    p.apellido2,  
+    p.numeroCedula, 
+    p.contrasena,  
+FROM
+    personas as p
